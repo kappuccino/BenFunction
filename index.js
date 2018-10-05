03a9c0273f3a78cf3e5067427560e8e86080e06f
Benjamin Mosnier <bm@kappuccino.org>
2018-10-05 21:45:31 +0200

    Change msg

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 exports.helloWorld = function helloWorld(req, res) {
-	const message = `Hello ${req.body.name}`
+	const message = `Hello ${req.body.name}, what the fuck bro ?`
 	res.status(200).send(message)
 }
